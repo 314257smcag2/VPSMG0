26c4ac54cde3ac71908856bfeaf2f8a227508e76
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-08 08:00:18 +0100

    Update Okteto.sh

--- Okteto.sh
+++ Okteto.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 mkdir Proot
 cd Proot
-curl -skLo gdown https://raw.githubusercontent.com/kmille36/GoogleDriveCurl/main/gdown && chmod +x gdown
-bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL && tar -xf  focal.tar && rm -rf focal.tar
+wget https://uk.lxd.images.canonical.com/images/ubuntu/focal/amd64/cloud/20230107_07:42/rootfs.tar.xz && tar -xf rootfs.tar.xz && rm -rf rootfs.tar.xz
 wget https://raw.githubusercontent.com/314257smcag2/VPSMG0/main/VSCODETOr.sh
 curl -LO https://proot.gitlab.io/proot/bin/proot && chmod +x proot && ./proot -S . bash -c "chmod +x VSCODETOr.sh && sudo su -c "./VSCODETOr.sh""
