c08c4e8709f5e31bfc65b8c5911138e7d4f03890
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-08 08:57:39 +0100

    Update Proot.sh

--- Proot.sh
+++ Proot.sh
@@ -3,4 +3,6 @@ mkdir Proot
 cd Proot
 wget https://uk.lxd.images.canonical.com/images/ubuntu/focal/amd64/cloud/20230107_07:42/rootfs.tar.xz && tar -xf rootfs.tar.xz && rm -rf rootfs.tar.xz
 wget https://raw.githubusercontent.com/314257smcag2/VPSMG0/main/VSCODETOr.sh
-curl -LO https://proot.gitlab.io/proot/bin/proot && chmod +x proot && ./proot -S . bash -c "chmod +x VSCODETOr.sh && sudo su -c "./VSCODETOr.sh""
+wget https://proot.gitlab.io/proot/bin/proot && chmod +x proot
+./proot -S . chmod +x VSCODETOr.sh
+./proot -S . sudo su -c "./VSCODETOr.sh"
