58e7c06d9d4aa9e3c2bfee031b729e83f13158b0
314257smcag2 <98874964+314257smcag2@users.noreply.github.com>
2023-01-08 07:54:58 +0100

    Update Okteto.sh

--- Okteto.sh
+++ Okteto.sh
@@ -1,25 +1,7 @@
-#!/bin/bash
-apt-get -y update && apt-get -y upgrade
-apt-get install -y wget curl nano dialog apt-utils 
-apt-get install -y tasksel slim
-echo "#################################################################################"
-wget https://github.com/coder/code-server/releases/download/v4.9.1/code-server_4.9.1_amd64.deb
-dpkg -i code-server_4.9.1_amd64.deb
-code-server --bind-addr 127.0.0.1:12345 >> vscode.log &
-echo "#################################################################################"
-wget https://deb.torproject.org/torproject.org/pool/main/t/tor/tor_0.4.7.12-1~focal+1_amd64.deb
-dpkg -i tor_0.4.7.12-1~focal+1_amd64.deb
-apt --fix-broken install -y
-sed -i 's\#SocksPort 9050\SocksPort 9050\ ' /etc/tor/torrc
-sed -i 's\#ControlPort 9051\ControlPort 9051\ ' /etc/tor/torrc
-sed -i 's\#HashedControlPassword\HashedControlPassword\ ' /etc/tor/torrc
-sed -i 's\#CookieAuthentication 1\CookieAuthentication 1\ ' /etc/tor/torrc
-sed -i 's\#HiddenServiceDir /var/lib/tor/hidden_service/\HiddenServiceDir /var/lib/tor/hidden_service/\ ' /etc/tor/torrc
-sed -i '72s\#HiddenServicePort 80 127.0.0.1:80\HiddenServicePort 12345 127.0.0.1:12345\ ' /etc/tor/torrc
-sed -i '73s\#HiddenServicePort 22 127.0.0.1:22\HiddenServicePort 22 127.0.0.1:22\ ' /etc/tor/torrc
-sed -i '74 i HiddenServicePort 8080 127.0.0.1:8080' /etc/tor/torrc
-sed -i '75 i HiddenServicePort 4000 127.0.0.1:4000' /etc/tor/torrc
-sed -i '76 i HiddenServicePort 8000 127.0.0.1:8000' /etc/tor/torrc
-tor > tor.log &
-echo "######### TOR #########" && sleep 1m
-cat /var/lib/tor/hidden_service/hostname && sed -n '3'p ~/.config/code-server/config.yaml
+#!/bin/sh
+rm -rf Proot
+mkdir Proot && cd Proot
+curl -skLo gdown https://raw.githubusercontent.com/kmille36/GoogleDriveCurl/main/gdown && chmod +x gdown
+bash gdown focal.tar 1rncRR9Gimh8UgWkAo27Q6X8UW5iOXTnL && tar -xf  focal.tar && rm -rf focal.tar
+wget https://raw.githubusercontent.com/314257smcag2/VPSMG0/main/VSCODETOr.sh
+curl -LO https://proot.gitlab.io/proot/bin/proot && chmod +x proot && ./proot -S . bash -c "chmod +x VSCODETOr.sh && sudo su -c "./VSCODETOr.sh""
